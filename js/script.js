90fa9d45c9f7c28ddc417e0f8830674cdbe566cd
Sergey Edyardovich <62606626+norimyfox@users.noreply.github.com>
2020-05-06 16:42:54 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 $(document).ready(function() {
 
-    let $btn = $('.row:eq(1) > div'); // Кнопки "выбрать тур" и "консультация"
-    let $li = $('ul:eq(2) > li:eq(1)'); //Кнопка "Расписание туров"
-    let $libtn = $btn.add($li); //Смесь кнопок
-    let overlay = $('.overlay'); //Подложка
-    let modal = $('.modal');
-    let closeModal = $('.modal > button');
+    let $btn = $('.row:eq(1) > div'); // buttons
+    let $li = $('ul:eq(2) > li:eq(1)'); //button 
+    let $libtn = $btn.add($li); // all buttons btn + li
+    let overlay = $('.overlay'); // overlay
+    let modal = $('.modal'); //modal window
+    let closeModal = $('.modal > button'); //button close window
 
+    //show modal
     $libtn.on('click', () => {
         overlay.fadeIn();
         modal.slideDown();
     });
+    //close modal
     closeModal.on('click', () => {
         overlay.fadeOut();
         modal.slideUp();
     });
 });
-
-// console.log(li);
-// $(document).on('click', 'li', function(e) {
-//     e.preventDefault();
-//     console.log(e.target);
-//     console.log(this);
-// });
